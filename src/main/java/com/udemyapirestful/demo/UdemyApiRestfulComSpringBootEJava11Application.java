8cc3b40b12bef4aed74555362a13311b0eb1b353
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2023-04-19 00:23:26 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	novo ficheiro: .dockerignore
    	novo ficheiro: .editorconfig
    	novo ficheiro: .gitattributes
    	novo ficheiro: .gitignore
    	novo ficheiro: .travis.yml
    	novo ficheiro: LICENSE
    	novo ficheiro: mvnw
    	novo ficheiro: mvnw.cmd
    	novo ficheiro: pom.xml
    	novo ficheiro: src/main/java/com/udemyapirestful/demo/UdemyApiRestfulComSpringBootEJava11Application.java
    	novo ficheiro: src/main/resources/application.properties
    	novo ficheiro: src/test/java/com/udemyapirestful/demo/UdemyApiRestfulComSpringBootEJava11ApplicationTests.java

---
+++ src/main/java/com/udemyapirestful/demo/UdemyApiRestfulComSpringBootEJava11Application.java
@@ -0,0 +1,14 @@
+package com.udemyapirestful.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiRestfulComSpringBootEJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiRestfulComSpringBootEJava11Application.class, args);
+		System.out.println("API RESTful com Spring Boot e Java 11 - Guia de Referência...");
+	}
+
+}
