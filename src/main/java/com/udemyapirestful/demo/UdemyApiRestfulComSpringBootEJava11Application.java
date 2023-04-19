6145b2f61634868d0c3d4fa2e421baeebc38b24a
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2023-04-19 00:36:14 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	modificado:    src/main/java/com/udemyapirestful/demo/UdemyApiRestfulComSpringBootEJava11Application.java

--- src/main/java/com/udemyapirestful/demo/UdemyApiRestfulComSpringBootEJava11Application.java
+++ src/main/java/com/udemyapirestful/demo/UdemyApiRestfulComSpringBootEJava11Application.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UdemyApiRestfulComSpringBootEJava11Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UdemyApiRestfulComSpringBootEJava11Application.class, args);
-		System.out.println("API RESTful com Spring Boot e Java 11 - Guia de Referência...");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyApiRestfulComSpringBootEJava11Application.class, args);
+        System.out.println("API RESTful com Spring Boot e Java 8 - Guia de Referência...");
+        System.out.println("API RESTful com Spring Boot e Java 11 - Guia de Referência...");
+    }
 
 }
