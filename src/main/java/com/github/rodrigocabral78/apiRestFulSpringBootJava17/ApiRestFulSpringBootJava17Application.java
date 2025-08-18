40e6a13e480d2cc3e67a41125230bc3213e51b47
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2025-08-18 19:10:45 -0400

    No ramo develop
    * Mudanças a serem submetidas:
    	new file:   .env
    	modified:   pom.xml
    	modified:   src/main/java/com/github/rodrigocabral78/apiRestFulSpringBootJava17/ApiRestFulSpringBootJava17Application.java
    	modified:   src/main/resources/application.properties
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>

--- src/main/java/com/github/rodrigocabral78/apiRestFulSpringBootJava17/ApiRestFulSpringBootJava17Application.java
+++ src/main/java/com/github/rodrigocabral78/apiRestFulSpringBootJava17/ApiRestFulSpringBootJava17Application.java
@@ -1,12 +1,24 @@
 package com.github.rodrigocabral78.apiRestFulSpringBootJava17;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiRestFulSpringBootJava17Application {
+  @Value("${paginate.per_page}")
+  private int perPage;
 
-	public static void main(String[] args) {
+  @Bean
+  public CommandLineRunner commandLineRunner() {
+    return args -> {
+      System.out.println("### Quantidade de elementos por página = " + this.perPage);
+    };
+  }
+
+  public static void main(String[] args) {
     SpringApplication.run(ApiRestFulSpringBootJava17Application.class, args);
     System.out.println("API RESTful com Spring Boot e Java 8 - Guia de Referência...");
     System.out.println("API RESTful com Spring Boot e Java 11 - Guia de Referência...");
@@ -14,4 +26,5 @@ public class ApiRestFulSpringBootJava17Application {
     System.out.println("API RESTful com Spring Boot e Java 21 - Guia de Referência...");
     System.out.println("API RESTful com Spring Boot e Java 24 - Guia de Referência...");
   }
+
 }
